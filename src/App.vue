02e1db5abc50279c30b4a332128f2f6e47e80f31
Ben Kuster <bkuster@vc.systems>
2024-09-16 15:01:01 +0200

    mimimal repor

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <v-app>
+    <v-main>
+      <HelloWorld />
+    </v-main>
+  </v-app>
+</template>
+
+<script lang="ts">
+  import { defineComponent } from 'vue';
+  import { VApp, VMain } from 'vuetify/components';
+
+  export default defineComponent({
+    components: {
+      VApp,
+      VMain,
+    },
+  });
+</script>
